b783ba7a4aaa8fd4e2cdd80bba52309e933001d6
Mykyta Uginchus <uginchus@gmail.com>
2023-11-10 23:53:58 +0200

    Create player and enemy classes

--- Source/Xena/Private/Character/XenaCharacterBase.cpp
+++ Source/Xena/Private/Character/XenaCharacterBase.cpp
@@ -7,6 +7,9 @@ AXenaCharacterBase::AXenaCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AXenaCharacterBase::BeginPlay()

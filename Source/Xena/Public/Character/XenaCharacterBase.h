3300b577df50dbccf28ad56baa0a20141b59667b
Mykyta Uginchus <uginchus@gmail.com>
2023-11-10 23:10:45 +0200

    Create Base Charatcer Class

---
+++ Source/Xena/Public/Character/XenaCharacterBase.h
@@ -0,0 +1,29 @@
+// Copyright Uginchus Education
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "XenaCharacterBase.generated.h"
+
+UCLASS()
+class XENA_API AXenaCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AXenaCharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};

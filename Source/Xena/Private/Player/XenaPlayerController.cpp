fe61468c218f1fd6a6171555cfa2dfcc4399688e
Mykyta Uginchus <uginchus@gmail.com>
2023-11-11 09:14:25 +0200

    Create move input actions and character mapping context // create playerc controller class

---
+++ Source/Xena/Private/Player/XenaPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright Uginchus Education
+
+
+#include "Player/XenaPlayerController.h"
+
---
+++ Source/Xena/Public/Player/XenaPlayerController.h
@@ -0,0 +1,14 @@
+// Copyright Uginchus Education
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "XenaPlayerController.generated.h"
+
+UCLASS()
+class XENA_API AXenaPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
